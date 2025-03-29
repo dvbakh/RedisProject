8e980366ce5b467404767c6f4b9a7c4eb47829da
Dmitry <90257559+dvbakh@users.noreply.github.com>
2025-03-29 12:17:13 +0300

    Update load_quotes.py
    removed unused imports

--- load_quotes.py
+++ load_quotes.py
@@ -2,16 +2,7 @@ import random
 import redis
 import requests
 from bs4 import BeautifulSoup
-import nltk
-from nltk.sentiment import SentimentIntensityAnalyzer
 
-# Загрузка необходимых моделей для NLP
-nltk.download('vader_lexicon')
-nltk.download("punkt")
-
-sia = SentimentIntensityAnalyzer()
-
-# Подключение к Redis
 redis_client = redis.Redis(host='localhost', port=6379, decode_responses=True)
 
 CATEGORIES = [
@@ -24,7 +15,6 @@ CATEGORIES = [
 
 MOODS = ["positive", "negative", "neutral"]
 
-# Функция для получения цитат с сайта (пример для https://quotes.toscrape.com/)
 def get_quotes():
     url = "https://statham.fun/"
     response = requests.get(url)
@@ -38,15 +28,15 @@ def get_quotes():
     print(f'Quotes got: {len(quotes)}')
     return quotes
 
-# Функция для определения настроения цитаты
+# В итоге просто случайное определение
 def choose_mood_and_tag():
     tag = random.choice(CATEGORIES)
     mood = random.choice(MOODS)
     return tag, mood
 
-# Основная функция обработки
 def process_quotes():
-    redis_client.flushdb()  # Очищаем Redis перед загрузкой
+    # Очищаем Redis перед загрузкой
+    redis_client.flushdb()
 
     quotes = get_quotes()
 
@@ -61,14 +51,13 @@ def process_quotes():
             "sentiment": sentiment
         })
 
-        # Добавляем идентификатор цитаты в множество для соответствующей категории
+        # Добавляем id цитаты в множество для соответствующей категории
         redis_client.sadd(f"category:{category}", quote_id)
 
-        # Добавляем идентификатор цитаты в множество для соответствующей тональности
+        # Добавляем id цитаты в множество для соответствующей тональности
         redis_client.sadd(f"sentiment:{sentiment}", quote_id)
 
     print(f"Загружено {len(quotes)} цитат в Redis.")
 
-# Запуск скрипта
 if __name__ == "__main__":
     process_quotes()
