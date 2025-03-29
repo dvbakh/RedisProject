b251822dec271fa32bacb0cce8571a1c2f1fee3d
Dmitry <90257559+dvbakh@users.noreply.github.com>
2025-03-29 12:19:50 +0300

    Update redis_db.py
    removed unused import & comms

--- redis_db.py
+++ redis_db.py
@@ -3,9 +3,8 @@ import random
 
 redis_client = redis.Redis(host="localhost", port=6379, decode_responses=True)
 
-# Функция для получения всех цитат по категории
 def get_quotes_by_category(category):
-    # Получаем все идентификаторы цитат для категории из Redis Set
+    # Получаем все id цитат для категории из Redis Set
     quote_ids = redis_client.smembers(f"category:{category}")
 
     quotes = []
@@ -15,7 +14,6 @@ def get_quotes_by_category(category):
         quote_data = redis_client.hgetall(quote_id)
         print(f"Data for {quote_id}: {quote_data}")
 
-        # Добавляем их в список
         if quote_data:
             quotes.append({
                 'id': quote_id,
@@ -28,9 +26,8 @@ def get_quotes_by_category(category):
 
     return quotes
 
-# Функция для получения случайной цитаты
 def get_random_quote():
-    # Получаем все ключи с цитатами
+    # Получаем все ключи
     keys = redis_client.keys(f"quote:*")
 
     # Выбираем случайный
@@ -46,7 +43,7 @@ def get_random_quote():
     }
 # Функция для получения случайных 10 цитат (для начального экрана)
 def get_random_quotes(num_quotes=10):
-    # Получаем все ключи с цитатами
+
     keys = redis_client.keys("quote:*")
 
     random_keys = random.sample(keys, min(num_quotes, len(keys)))
